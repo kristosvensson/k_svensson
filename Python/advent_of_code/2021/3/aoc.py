4e99eef168b4ac2f031f90cb96831c5daf7aef60
Kristofer Svensson <Kristofer@Kristofers-MacBook-Pro.local>
2021-12-11 22:01:57 +0100

    Working on AoC 3. A week late, but better safe than sorry.

--- Python/advent_of_code/2021/3/aoc.py
+++ Python/advent_of_code/2021/3/aoc.py
@@ -1,33 +1,42 @@
+#!/usr/local/bin/python3.8
+
 import statistics as st
 
-f = open('data_3.txt', 'r')
-data = []
+def get(url):
+    f = open(url, 'r')
+    data = []
 
-for x in f:
-    data.append(x.strip())
+    for x in f:
+        data.append(x.strip())
+    return data
 
-mode = []
-anti_mode = []
+data = get('aoc_input_3.txt')
 
-for i in range(0,len(data[1])):
-    tmp = []
-    for x in data:
-        tmp.append(x[i])
-    mode.append(int(st.mode(tmp)))
+def get_mode(data):
+    mode = []
+    for i in range(0,len(data[0])):
+        tmp = []
+        for x in data:
+            tmp.append(x[i])
+        mode.append(int(st.mode(tmp)))
 
+    return mode
+
+mode = get_mode(data)
 print(mode)
 
-for i in range(0,len(mode)):
-    if(mode[i] == 1):
-        anti_mode.append(0)
-    else:
-        anti_mode.append(1)
+def get_anti_mode(mode):
+    anti_mode = []
+    for i in range(0,len(mode)):
+        if(mode[i] == 1):
+            anti_mode.append(0)
+        else:
+            anti_mode.append(1)
 
+    return anti_mode
+
+anti_mode = get_anti_mode(mode)
 print(anti_mode)
 
-str_mode = ''
-str_anti_mode = ''
 
-tmp = []
-for i in data:
-    tmp.append(i[0])
+
