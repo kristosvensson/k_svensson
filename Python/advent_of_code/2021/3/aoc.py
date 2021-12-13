2f472e3654fb0a1ae249515d8200c842bf79fe2d
Kristofer Svensson <Kristofer@Kristofers-MacBook-Pro.local>
2021-12-13 18:52:04 +0100

    finished part 3 of AoC

--- Python/advent_of_code/2021/3/aoc.py
+++ Python/advent_of_code/2021/3/aoc.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3.8
 
-import statistics as st
+import numpy as np
 
 def get(url):
     f = open(url, 'r')
@@ -10,20 +10,38 @@ def get(url):
         data.append(x.strip())
     return data
 
-data = get('aoc_input_3.txt')
+def get_mode_i(data, i):
+    tmp = []
+    
+    for x in data:
+        tmp.append(int(x[i]))
+    if np.mean(tmp) >= 0.5:
+        return 1
+    else:
+        return 0
 
+def get_anti_mode_i(data, i):
+    tmp = []
+    for x in data:
+        tmp.append(int(x[i]))    
+    if np.mean(tmp) < 0.5:
+        return 1
+    else:
+        return 0
+        
 def get_mode(data):
     mode = []
     for i in range(0,len(data[0])):
         tmp = []
         for x in data:
-            tmp.append(x[i])
-        mode.append(int(st.mode(tmp)))
+            tmp.append(int(x[i]))
+        if np.mean(tmp) >= 0.5:
+            mode_tmp = 1
+        else:
+            mode_tmp = 0
+        mode.append(mode_tmp)
 
     return mode
-
-mode = get_mode(data)
-print(mode)
 
 def get_anti_mode(mode):
     anti_mode = []
@@ -35,8 +53,53 @@ def get_anti_mode(mode):
 
     return anti_mode
 
-anti_mode = get_anti_mode(mode)
-print(anti_mode)
+def calc_bin(data):
+    num = 0
+    for i in range(0,len(data)):
+            num += (data[-(i+1)]) * 2 ** i
+    return num
 
+def filter_i(data, i, num):
+    tmp_data = []
+    for x in data:
+        if int(x[i]) == num:
+            tmp_data.append(x)
+    return tmp_data
+    
+def find_oxy(data):
+    tmp_filter = filter_i(data, 0, get_mode_i(data, 0))
+    for i in range(1,len(data[0])):
+        if len(tmp_filter) == 1:
+            break
+        else:
+            tmp_filter = filter_i(tmp_filter, i, get_mode_i(tmp_filter, i))
+    tmp = []
+    for i in range(0,len(tmp_filter[0])):
+        tmp.append(int(tmp_filter[0][i]))
+    return tmp
 
+def find_co(data):
+    tmp_filter = filter_i(data, 0, get_anti_mode_i(data, 0))
+    for i in range(1,len(data[0])):
+        if len(tmp_filter) == 1:
+            break
+        else:
+            tmp_filter = filter_i(tmp_filter, i, get_anti_mode_i(tmp_filter, i))
+    tmp = []
+    for i in range(0,len(tmp_filter[0])):
+        tmp.append(int(tmp_filter[0][i]))
+    return tmp
 
+def main():
+    data = get('aoc_input_3.txt')
+    mode = get_mode(data)
+    anti_mode = get_anti_mode(mode)
+    print("Power consumption:",calc_bin(mode)*calc_bin(anti_mode))    
+    oxy = find_oxy(data)
+    co = find_co(data)
+    print("Oxygen generator rating:", calc_bin(oxy))
+    print("CO2 scrubber rating:", calc_bin(co))
+    print("Life support rating:", calc_bin(oxy)*calc_bin(co))
+    
+    
+main()
