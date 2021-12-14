312e14573a7e84c2363372c6090f307a18a0d9ab
Kristofer Svensson <Kristofer@Kristofers-MacBook-Pro.local>
2021-12-14 21:19:40 +0100

    working on it

--- Python/advent_of_code/2021/4/AoC_4.py
+++ Python/advent_of_code/2021/4/AoC_4.py
@@ -4,9 +4,25 @@ import numpy as np
 
 def read_drawn_numbers(file):
     
-    with open(file) as f:
-        num = f.readline().split(',')
-        return num
-
-print(read_drawn_numbers('game_input.txt'))
+    drawn_num = [int(num) for num in np.loadtxt(file, dtype = 'str', delimiter = ',',max_rows = 1)]
+    return drawn_num
     
+def read_matrices(file, i):
+    
+    array = [np.loadtxt(file, skiprows=2, dtype = 'int')]
+    matrices = np.reshape(array,(i,5))
+    matrices = np.vsplit(matrices, 100)
+    return matrices
+    
+print(read_drawn_numbers('../4/game_input.txt'))
+
+mat = read_matrices('../4/game_input.txt',500)
+
+for array in mat:
+    print(array[:,3])
+
+
+a =[1,2,3]
+b = [5,4,3,2,1]
+
+a=b
