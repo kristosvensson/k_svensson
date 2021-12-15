31ce5bd9b5caa24d74bb0d01f93084733334576f
Kristofer Svensson <Kristofer@Kristofers-MacBook-Pro.local>
2021-12-15 22:05:02 +0100

    still tryin'

--- Python/advent_of_code/2021/4/AoC_4.py
+++ Python/advent_of_code/2021/4/AoC_4.py
@@ -7,22 +7,38 @@ def read_drawn_numbers(file):
     drawn_num = [int(num) for num in np.loadtxt(file, dtype = 'str', delimiter = ',',max_rows = 1)]
     return drawn_num
     
-def read_matrices(file, i):
+def read_boards(file):
     
     array = [np.loadtxt(file, skiprows=2, dtype = 'int')]
-    matrices = np.reshape(array,(i,5))
+    matrices = np.reshape(array,(500, 5))
     matrices = np.vsplit(matrices, 100)
     return matrices
     
-print(read_drawn_numbers('../4/game_input.txt'))
+#print(read_drawn_numbers('../4/game_input.txt'))
+#print(read_boards('../4/game_input.txt'))
 
-mat = read_matrices('../4/game_input.txt',500)
-
-for array in mat:
-    print(array[:,3])
-
-
-a =[1,2,3]
-b = [5,4,3,2,1]
-
-a=b
+def main():
+    
+    #Load data
+    drawn = read_drawn_numbers('../4/game_input.txt')
+    mat = read_boards('../4/game_input.txt')
+    
+    #Initiate empty array for drawn numbers
+    tmp = []
+    run = True
+    ite = 0
+    
+    
+    while run:
+        for i in range(0,len(drawn)):  
+            tmp.append(drawn[i])  
+            for matrix in mat:
+                if sum(np.isin(matrix[1,:],tmp)) == 5:
+                    
+                    run = False
+                    break
+            ite += 1
+            
+    print(ite)
+    
+main()
